6193d4290a14c557dd4f09203dad7bf7ddeb904c
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-03-27 16:03:12 -0500

    herencia

---
+++ JavaApplication16/src/javaapplication16/Profesor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Profesor extends Persona {
+
+    public Profesor(String nombre, int telefono, String direccion) {
+        super(nombre, telefono, direccion);
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+}
